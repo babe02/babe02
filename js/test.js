5f734471967b5bcac3ae7481c695214c5098fc2c
bravo1900 <ceweithr@163.com>
2020-08-26 11:04:55 +0800

    first commit

---
+++ js/test.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+
+var minutes = 1000 * 60
+var hours = minutes * 60
+var days = hours * 24
+var years = days * 365
+var t = Date.parse("Jul 8, 2005")
+var y = t/years
+document.write("It's been: " + y + " years from 1970/01/01")
+document.write(" to 2005/07/08!")
+
+</script>
